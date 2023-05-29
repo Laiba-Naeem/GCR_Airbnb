42a1df89c271b4c782ed75119f758ae83f582b1a
laiba <laiba.naeem@mlsense.ai>
2023-05-29 15:07:34 +0500

    db

--- drf/settings.py
+++ drf/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = "drf.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        'ENGINE': 'django.db.backends.postgresql'
-        # "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+
+#         "NAME": BASE_DIR / "db.sqlite3"
+#     }
+# }
 
 
 # Password validation
@@ -151,16 +151,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = ["https://airbnb-laiba-2drk4lqwsa-uc.a.run.app"]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': os.getenv('HOST', None),
-#         'USER': os.getenv('USER', None),
-#         'PASSWORD': os.getenv('PASSWORD', None),
-#         'NAME': os.getenv('NAME', None)
-#         # 'PORT': os.getenv('PORT', None)
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.getenv('HOST', None),
+        'USER': os.getenv('USER', None),
+        'PASSWORD': os.getenv('PASSWORD', None),
+        'NAME': os.getenv('NAME', None),
+        'PORT': os.getenv('PORT', None)
+    }
+}
 
 # DATABASES = {
 #     'default': {
